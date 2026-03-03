c9a0daab7ba8b56f30cfb156d01fecaff460b87b
Jathin-Seerla <jathin.seerla@gmail.com>
2026-03-03 12:49:27 -0500

    terraform

--- terraform-module-development/vpc-module-test/provider.tf
+++ terraform-module-development/vpc-module-test/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "1-remote-state"
-    key    = "workspace-demo"
+    key    = "vpc-test"
     region = "us-east-1"
     encrypt        = true
     use_lockfile = true
@@ -17,4 +17,5 @@ terraform {
 
 provider "aws" {
   # Configuration options
+  region = "us-east-1"
 }
